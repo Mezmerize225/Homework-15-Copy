9a658ca832116c204887e0b65a002690f254c466
Mez <Tam@yahoo.com>
2025-09-19 00:53:46 +0400

    homework15/part2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ButtonComponent } from './components/button/button.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ButtonComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  // styleUrl: './app.component.scss'
+  styles: ['.row{display: flex; justify-content: space-between; margin-bottom: 16px}']
 })
 export class AppComponent {
   title = 'my-first-app';
---
+++ src/app/components/button/button.component.ts
@@ -0,0 +1,15 @@
+import { Component, input } from '@angular/core';
+import { buttonColor } from './models/button-color-model';
+import { NgClass } from '@angular/common';
+
+@Component({
+  selector: 'app-button',
+  imports: [NgClass],
+  templateUrl: './button.component.html',
+  styleUrl: './button.component.scss'
+})
+export class ButtonComponent {
+  public buttonColorType = input<buttonColor>('primary');
+}
+
+
