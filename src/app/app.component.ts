502592c9bbb06e341b5b09b3f6b7cd39f73a61cd
Mez <Tam@yahoo.com>
2025-09-19 19:12:04 +0400

    homework 15 copy/part4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,12 +6,28 @@ import { ButtonComponent } from './components/button/button.component';
   selector: 'app-root',
   imports: [ButtonComponent],
   templateUrl: './app.component.html',
-  // styleUrl: './app.component.scss'
-  styles: `.row{
-  display: flex; 
-  justify-content: space-between; 
-  margin-bottom: 16px
-  }`
+  styleUrl: './app.component.scss'
+  // styles: `.row{
+  // display: flex; 
+  // justify-content: space-between; 
+  // margin-bottom: 16px
+  // }
+  // .header-text {
+  //   font-weight: 700;
+  //   font-size: 30px;
+  //   line-height: 36px;
+  //   color: #0F172A;
+  //   text-align: center;
+  // }
+  // .gradient-line {
+  //   width: 96px;
+  //   height: 4px;
+  //   border-radius: 9999px;
+  //   border: 1px solid #E5E7EB;
+  //   background-image: linear-gradient(to right, #3B82F6, #9333EA);
+  //   display: flex;
+  //   justify-content: center;
+  //   }`
 })
 export class AppComponent {
   title = 'my-first-app';
